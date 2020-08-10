f7b0ad595497d94a5593f3d2036a3aa280a9ad95
Saeed Shareef <saeedhshareef@gmail.com>
2020-08-10 00:10:41 -0400

    Updates

---
+++ precache-manifest.2fb63c2ebca363def680a712abcc1824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073b168be13f3f6e8914454731b1e87a",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "62c0b179fbc762dd3e08",
+    "url": "/todoList/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f53b75fc634aca28f68e",
+    "url": "/todoList/static/js/2.1435bc30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoList/static/js/2.1435bc30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c0b179fbc762dd3e08",
+    "url": "/todoList/static/js/main.865d4d91.chunk.js"
+  },
+  {
+    "revision": "c6f746245a04408a3ccd",
+    "url": "/todoList/static/js/runtime-main.2b418a56.js"
+  }
+]);
